95c051fa24e7f472d81c71d76ee5a095757a0f50
melekladh <ladharimalek10@gmail.com>
2025-08-07 14:53:18 +0100

    second commit

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ from utils.summarize import summarize
 
 if  __name__=="__main__":
     path = "data/audio11.mp4"
-    ARTICLE = 
     result = extract(path)
     transcript = result["text"]                
-        
+
+
+    
     timestamps=extract_segments(result)
     tokenized=tokenizer(transcript)
     stemmed = stemmer(tokenized)
     
-    summary=summarize(ARTICLE)
-    my_object=build(transcript,stemmed,timestamps)
+    summarized=summarize(transcript)
+    my_object=build(transcript,stemmed,timestamps,summarized)
 
 
     print(my_object)
--- utils/summarize.py
+++ utils/summarize.py
@@ -2,6 +2,5 @@ from transformers import pipeline
 def summarize (ARTICLE):
 
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-    return (summarizer(ARTICLE, max_length=130, min_length=30, do_sample=False))
-
-
+    summarized= (summarizer(ARTICLE, max_length=130, min_length=30, do_sample=False))
+    return summarized
